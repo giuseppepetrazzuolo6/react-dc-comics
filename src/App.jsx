bf0adde3830ef01fd856817fb970e879f59a6917
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-04 13:40:26 +0100

    add header in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import logo from './assets/img/dc-logo.png'
+
 function App() {
-  
   return (
     <>
+      <header>
+        <div className="d-flex justify-content-around py-3">
+          <img width={70} className="logo" src={logo} alt="Logo DC" />
+          <nav class="navbar navbar-expand navbar-light">
+            <div class="nav navbar-nav">
+              <a class="nav-item nav-link" href="#">Characters</a>
+              <a class="nav-item nav-link border-bottom border-4 border-primary" href="#">Comics</a>
+              <a class="nav-item nav-link" href="#">Movie</a>
+              <a class="nav-item nav-link" href="#">TV</a> 
+              <a class="nav-item nav-link" href="#">Games</a>
+              <a class="nav-item nav-link" href="#">Collectibles</a>
+              <a class="nav-item nav-link" href="#">Videos</a>
+              <a class="nav-item nav-link" href="#">Fans</a>
+              <a class="nav-item nav-link" href="#">News</a>
+              <a class="nav-item nav-link" href="#">Shop</a>
+            </div>
+          </nav> 
+        </div>
+      </header>
+
+      <main></main>
+      <footer></footer>
     </>
   )
 }
