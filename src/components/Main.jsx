3c6a2e08397f84dc4497eca4c1b3a6ece71ba5ca
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-06 12:08:56 +0100

    added props to Card component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import Card from './Card'
+import ComicsList from './ComicsList'
 import Jumbotron from './Jumbotron'
 import digital from '../assets/img/buy-comics-digital-comics.png'
 import merch from '../assets/img/buy-comics-merchandise.png'
@@ -6,12 +6,13 @@ import sub from '../assets/img/buy-comics-subscriptions.png'
 import shop from '../assets/img/buy-comics-shop-locator.png'
 import visa from '../assets/img/buy-dc-power-visa.svg'
 
+
 export default function Main() {
 
     return (
         <main>
             <Jumbotron />
-            <Card />
+            <ComicsList />
             <section className="main-bottom bg-primary">
                 <div className="container d-flex align-items-center justify-content-around">
                     <div className="d-flex align-items-center">
